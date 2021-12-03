38c338242aac79cb07f8c1bee3f44a7f16bf5477
NeverSnevertje <neversnevertje@gmail.com>
2021-12-03 16:50:17 +0100

    3 files modified

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'generator-component',
   templateUrl: './generator.component.html',
-  styleUrls: ['./generator.component.css']
+  styleUrls: ['./generator.component.css'],
 })
 export class GeneratorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  steden: string[] = ['Deventer','Amsterdam'];
+  deStad: string;
+
+  geefStad() {
+   var rand =this.getRandomInt(0,1)
+   
+    this.deStad= this.steden[rand];
+
   }
 
+  getRandomInt(min, max) : number{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+  }
 }
